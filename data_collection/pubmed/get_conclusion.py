57877e8153898ead2c851553dbcc26cd78a6d73e
nikob <nikolaj.bauer@uzh.ch>
2023-08-29 10:54:53 +0200

    add script to filter pubmed articles for sufficiently long conclusions

---
+++ data_collection/pubmed/get_conclusion.py
@@ -0,0 +1,34 @@
+from lxml import etree
+import argparse
+from tqdm import tqdm
+import os
+
+
+
+def extract_conclusion(infilepath, outfolder):
+    xml = etree.parse(infilepath)
+    articles = xml.xpath("//article[//sec[title='Conclusion']]")
+    for article in articles:
+        text = ''.join(article.xpath(".//sec[title='Conclusion']//text()"))  # getting the conlcusion as string
+        num_toks = len(text.split())
+        if num_toks > 500:
+            title = article.xpath(".//title-group/article-title/text()")[0]
+            pmid = article.xpath(".//article-id[@pub-id-type='pmid']/text()")[0]
+            year = article.xpath(".//pub-date[1]/year/text()")[0]
+            outfilepath = os.path.join(outfolder, f"{year}-{pmid}_{num_toks}_en.txt")
+            with open(outfilepath, "w", encoding="utf-8") as outfile:
+                outfile.write(f"{title}\n\n{text}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infolder")
+    parser.add_argument("outfolder")
+    args = parser.parse_args()
+    infolder = args.infolder
+    outfolder = args.outfolder
+
+    infilepaths = [os.path.join(infolder, filename) for filename in os.listdir(infolder)]
+    inputs = [(infilepath, outfolder) for infilepath in infilepaths]
+    for input in tqdm(inputs):
+        extract_conclusion(input[0], input[1])
+
